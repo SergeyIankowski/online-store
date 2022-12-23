3525899df7d8712c46a608094d70fcb41e9ee47f
SeregeyIankowski <garret933@gmail.com>
2022-12-23 19:48:31 +0300

    fix: fix interfaces index.ts

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,3 +1,5 @@
 import LoaderInterface from './loader-interface';
+import { IncomeData } from './incomeData';
+import { ProductData } from './productData';
 
 export { IncomeData, ProductData, LoaderInterface };
