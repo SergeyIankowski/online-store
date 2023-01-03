c32d40a6ddd0c2d741a3f8bc1f5b101e6a1158ae
ImmelstronDev <wishandra.afk@gmail.com>
2023-01-03 13:58:52 +0300

    feat: added revers function for sort items

--- src/utils/get_sort_items.ts
+++ src/utils/get_sort_items.ts
@@ -4,20 +4,20 @@ import { ProductData } from "../interfaces/productData";
 export let renderList:ProductData[] = [];
 
 export function getSortItemsByCategory(data:IncomeData, evt: string): void {
-const newData = data.products.filter((el:ProductData) => el.category === evt)
-newData.forEach((el:ProductData) => renderList.push(el))
-console.log(renderList)
+    const newData = data.products.filter((el:ProductData) => el.category === evt)
+    newData.forEach((el:ProductData) => renderList.push(el))
+    console.log(renderList)
 };
 
 export function getSortItemsByBrand(data:IncomeData, evt: string): void {
-const newData = data.products.filter((el:ProductData) => el.brand === evt)
-newData.forEach((el:ProductData) => renderList.push(el))
-console.log(renderList)
+    const newData = data.products.filter((el:ProductData) => el.brand === evt)
+    newData.forEach((el:ProductData) => renderList.push(el))
+    console.log(renderList)
 };
 
 
 
-export function getSortItemsByPrice(data: IncomeData, field: 'price'|'rating'|'discountPercentage'): void {
+export function getSortItems(data: IncomeData, field: 'price'|'rating'|'discountPercentage'): void {
     const newData = data.products.sort(function (x: ProductData, y: ProductData): number {
         if (x[field] < y[field]) {
         return -1;
@@ -27,3 +27,14 @@ export function getSortItemsByPrice(data: IncomeData, field: 'price'|'rating'|'d
     });
     console.log(newData);
 };
+
+export function getSortItemsReverse(data: IncomeData, field: 'price'|'rating'|'discountPercentage'): void {
+    const newData = data.products.sort(function (x: ProductData, y: ProductData): number {
+        if (x[field] > y[field]) {
+        return -1;
+        } else {
+        return 1
+        }
+    });
+    console.log(newData);
+}
